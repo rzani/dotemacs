a5fc44aeaa6e84d9227e77ac9c00f573d528d5aa
Rodrigo Zani <rodrigo.zhs@gmail.com>
2017-01-13 09:42:03 -0200

    First commit

---
+++ init.el
@@ -0,0 +1,49 @@
+
+(let ((minver "23.3"))
+  (when (version<= emacs-version "23.1")
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+
+;; create constant to check OS
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+
+
+;;----------------------------------------------------------------------------
+;; Package manager
+;;----------------------------------------------------------------------------
+
+(require 'package)
+
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote nil))
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(package-selected-packages
+   (quote
+    (linum-relative helm-ag ag helm-projectile projectile rainbow-delimiters smartparens auto-complete avy org-bullets which-key try evil-leader evil helm use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum-relative-current-face ((t (:foreground "#a89984" :weight bold)))))
