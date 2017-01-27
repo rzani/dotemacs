2affc31d81b6750a4114bcc002ac82f106f4cf39
Rodrigo Zani <rodrigo.zhs@gmail.com>
2017-01-27 21:15:44 -0200

    add languages

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 (defconst *is-a-mac* (eq system-type 'darwin))
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 
-
 ;;----------------------------------------------------------------------------
 ;; Package manager
 ;;----------------------------------------------------------------------------
@@ -42,19 +41,18 @@
 				 (t default-color))))
 		(set-face-background 'mode-line (car color))
 		(set-face-foreground 'mode-line (cdr color))))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote nil))
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(package-selected-packages
    (quote
-    (flycheck web-mode magit linum-relative helm-ag ag helm-projectile projectile rainbow-delimiters smartparens auto-complete avy org-bullets which-key try evil-leader evil helm use-package))))
+    (evil-nerd-commenter atom-one-dark-theme emmet-mode php-mode dot-mode solarized-theme yaml-mode which-key web-mode use-package try smartparens rainbow-delimiters org-bullets nginx-mode magit linum-relative helm-projectile helm-ag flycheck evil-leader editorconfig color-theme-sanityinc-solarized avy auto-complete ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
