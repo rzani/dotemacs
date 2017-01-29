e50a326d660ce0d350dbe42672a63aaa0f425633
Rodrigo Zani <rodrigo.zhs@gmail.com>
2017-01-29 12:58:59 -0200

    separate custom and function

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(package-selected-packages
+   (quote
+    (nginx-mode yaml-mode web-mode editorconfig auto-complete flycheck emmet-mode magit rainbow-delimiters smartparens avy org-bullets which-key try linum-relative evil-nerd-commenter evil-leader evil-mc evil-matchit evil-surround evil projectile ag counsel smex solarized-theme color-theme-sanityinc-solarized emojify use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum-relative-current-face ((t (:foreground "#a89984" :weight bold)))))
--- init.el
+++ init.el
@@ -1,20 +1,10 @@
+;; @author rzani <rodrigo.zhs@gmail.com>
 
-(let ((minver "23.3"))
-  (when (version<= emacs-version "23.1")
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version<= emacs-version "24")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; create constant to check OS
-(defconst *is-a-mac* (eq system-type 'darwin))
-(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
-
-;;----------------------------------------------------------------------------
-;; Package manager
-;;----------------------------------------------------------------------------
+(require 'check-version)
 
 (require 'package)
-
 (setq package-enable-at-startup nil)
 
 (add-to-list 'package-archives
@@ -28,39 +18,9 @@
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
 
+(require 'functions)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
-;; change mode-line color by evil state
-(lexical-let ((default-color (cons (face-background 'mode-line)
-				   (face-foreground 'mode-line))))
-  (add-hook 'post-command-hook
-	    (lambda ()
-	      (let ((color (cond ((minibufferp) default-color)
-				 ((evil-insert-state-p) '("#ffc107" . "#ffffff"))
-				 ((evil-visual-state-p) '("#9c27b0" . "#ffffff"))
-				 ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
-				 ((buffer-modified-p)   '("#2196f3" . "#ffffff"))
-				 (t default-color))))
-		(set-face-background 'mode-line (car color))
-		(set-face-foreground 'mode-line (cdr color))))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(package-selected-packages
-   (quote
-    (evil-mc evil-matchit evil-surround dashboard emojify smex counsel swiper evil-nerd-commenter atom-one-dark-theme emmet-mode php-mode dot-mode solarized-theme yaml-mode which-key web-mode use-package try smartparens rainbow-delimiters org-bullets nginx-mode magit linum-relative helm-projectile helm-ag flycheck evil-leader editorconfig color-theme-sanityinc-solarized avy auto-complete ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(linum-relative-current-face ((t (:foreground "#a89984" :weight bold)))))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
